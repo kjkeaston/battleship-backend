18403b24329f5a61c508a1cf38e361d7cb4e3b52
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2018-02-01 18:46:56 -0800

    initial creation of backend directory and server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+let express = require('express');
+// db = require('./models'),
+let app = express();
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+let battleshipRouter = require('./config/routes.js');
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+//   next();
+// });
+
+app.use(battleshipRouter);
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log(`Listening on port ${ port }`);
+  console.log("Listening to Battleship-Backend");
+});
