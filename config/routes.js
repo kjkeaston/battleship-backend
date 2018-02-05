101828c595c29f6fd02e330d8d17a51973b5549c
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2018-02-05 09:44:12 -0800

    added routes, controllers to get code out of server.js

---
+++ config/routes.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var GamePageController = require('../controllers/GamePageBackend.js');
+
+//Game Routes
+router.get('/api/games', GamePageController.index);
+router.post('/api/games', GamePageController.create);
+router.get('/api/games/:game_id', GamePageController.show);
+router.put('/api/games/:game_id', GamePageController.update);
+router.delete('/api/games/:game_id', GamePageController.destroy);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({
 let models = require('./models');
 let Game = models.Game;
 
+let GamesRouter = require('./config/routes.js');
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -18,53 +20,11 @@ app.use(function(req, res, next) {
   next();
 });
 
-function allRowColumnPossibilities(gridSize) { //if 5x5, gridSize = 5
-  let numberPossibilitiesArray = Array.from(Array(gridSize).keys());
-  let allPossibilities = [];
-  numberPossibilitiesArray.map(oneRow => {
-    numberPossibilitiesArray.map(oneColumn => {
-      allPossibilities.push([oneRow, oneColumn]);
-    });
-  });
-  return allPossibilities;
-};
+app.use(GamesRouter);
 
-function chooseUniqueShips(gridSize, shipCount) {
-  let allPossibilities = allRowColumnPossibilities(gridSize);
-  let ships = [];
-  for (let i = 0; i < shipCount; i++) {
-    let randomIdx = Math.floor(Math.random() * allPossibilities.length); //gets a random index
-    element = allPossibilities[randomIdx] //selects element at that randomIdx
-    ships.push(element) //pushes element into ships array
-    allPossibilities.splice(randomIdx, 1) //cuts element from allPossibilities array
-  }
-  return ships
-}
-
-app.post('/api/games', function create(req, res) {
-  let p2ShipLocations = chooseUniqueShips(5, 3); //the gridSize and shipCount needs to be passed in as variables, currently hardcoded
-  let game = new Game ({
-    p1_positions: req.body.p1_positions,
-    p2_positions: [p2ShipLocations],
-    game_finished: false
-  })
-  game.save( function(err, game) {
-    if (err) res.send(err);
-    else res.json(game);
-    console.log("Created new game")
-  });
-});
-
-app.get('/api/games/:game_id', function show(req, res) {
-  Game.findById(req.params.game_id, function(err, game){
-    if (err) res.send(err);
-    else res.json(game);
-  });
-});
 
 let port = process.env.PORT || 3000;
 
 app.listen(port, function() {
-  console.log(`Listening on port ${ port }`);
-  console.log('Listening to Battleship-Backend');
+  console.log(`Listening on port ${ port } - Listening to Battleship-Backend`);
 });
