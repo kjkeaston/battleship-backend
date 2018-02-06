bb40dcd7990e8ce47eb4ff5f8582ba2a6b00325b
Eva <evaliisagalen@gmail.com>
2018-02-05 17:18:39 -0800

    reconfigured controllers/GamePageBackend.js update to accept a guess and return T or F

--- controllers/GamePageBackend.js
+++ controllers/GamePageBackend.js
@@ -1,5 +1,5 @@
 var models = require('../models');
-var Game = models.Game; 
+var Game = models.Game;
 
 // Get JSON view of all the games currently in DB
 function index(req, res) {
@@ -35,12 +35,27 @@ function chooseUniqueShips(gridSize, shipCount) {
   return ships
 }
 
+function isEqual(positions, guess) { // checks the equality of an array with 2 elements [1,2] and [1,2]
+  return positions[0] === guess[0] && positions[1] === guess[1]
+}
+
+function isHit(positionsArray, guess) { // checks if guess is in positions array
+  for(let i = 0; i < positionsArray.length; i++) {
+    let oneItem = positionsArray[i];
+      if (isEqual(oneItem, guess)) {
+        return true
+      }
+  }
+  return false
+}
+
+
 // Add new Game to DB on 'Enter' click
 function create(req, res) {
   let p2ShipLocations = chooseUniqueShips(5, 3); //the gridSize and shipCount needs to be passed in as variables, currently hardcoded
   let game = new Game ({
     p1_positions: req.body.p1_positions,
-    p2_positions: [p2ShipLocations],
+    p2_positions: p2ShipLocations,
     game_finished: false
   })
   game.save(function (err, game) {
@@ -55,23 +70,30 @@ function create(req, res) {
 
 // Get one game by game_id
 function show(req, res) {
-  Game.findById(req.params.game_id, function(err, game){
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(game);
-    }
+  Game.findOne({_id: req.params.game_id}, function(err, foundGame){
+    if (err) res.send(err);
+    else res.json(foundGame);
   });
 };
 
 function update(req, res) {
-  Game.findByIdAndUpdate(req.params.game_id, 
-    {$set: req.body}, {"new": true}, function(err, updatedGame) {
-    if(err) {
-      res.send(err);
-      console.log("error updating game", err);
+  let guess = req.body.p1_guesses
+  Game.findOne({_id: req.params.game_id}, function(err, foundGame){
+    let p2Pos = foundGame.p2_positions
+    if (err) res.send(err);
+    if (guess) {
+      let oneGuess = JSON.parse(guess);
+      if (isHit(p2Pos, oneGuess)) {
+        res.send(true)
+      } else {
+        res.send(false)
+      }
     } else {
-      res.json(updatedGame);
+      foundGame.p1_positions = req.body.p1_positions,
+      foundGame.save(function(err, saved) {
+        if(err) { console.log('error', err); }
+        res.json(saved);
+      })
     }
   });
 }
