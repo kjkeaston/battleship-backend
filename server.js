a31718f983e977607b368fcba725266e2baf847e
Xianyi555 <xianyi555@gmail.com>
2018-02-07 17:41:45 -0800

    CORS part 2

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
-  next();
+  // next();
+  if ('OPTIONS' === req.method) {
+    //respond with 200
+    res.send(200);
+  } else {
+    next();
+  }
 });
 
 app.options("/*", function(req, res, next){
